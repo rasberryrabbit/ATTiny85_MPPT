42b2600c9332304c30793d281e9e46a0cad7280b
rasberryrabbit <lovelust@hanmail.net>
2018-12-13 10:49:24 +0900

    set more optimized update interval.

--- sketch_oct18a.ino
+++ sketch_oct18a.ino
@@ -34,7 +34,7 @@
 #define INC_PWM_MAX 1
 #define ADC_MAX_LOOP 3  // 3~6
 #define INC_PWM_MIN 0
-#define _UPDATE_INT 20
+#define _UPDATE_INT 50
 #define _CUR_LIMIT 12  // 0.04V / 3.6 * 1024
 #define _UPDATE_VOL 1
 #define VOLMUL ((int)25/6)  // Voltage vs Current = 25V(1024) / 6A(1024)
